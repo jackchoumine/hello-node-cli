f2f75cf6b30e3da409dee837c8a7b1cda7ae1190
ZhoQiJun <zhouqijun4job@163.com>
2024-05-15 02:36:49 +0800

    脚手架原理

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
 #!/usr/bin/env node
-const clear = require('clear-any-console')
-const welcome = require('cli-welcome')
-const pkgJson = require('./package.json')
-const chalk = require('chalk') //NOTE 控制台彩色字体输出
-const log = console.log
-// console.clear() // 输出之前清除控制台
-// clear() // 兼容 3 大 os
-welcome({
-  title: pkgJson.name,
-  tagLine: 'Howdy,nice to meet ya',
-  description: pkgJson.description,
-  version: pkgJson.version,
-  bgColor: '#FADC00',
-  color: '#00000',
-  bold: true,
-  clear: true,
-})
-log(`${chalk.blue('My name is Jack!')}`)
+
+console.log('Hello, Jack!')
